9d65bc2df96b1ff1ce1a7239bdd4c848c65adecb
Joice Souza <joice_fsouza@hotmail.com>
2021-06-03 17:41:52 -0300

    começando a desenvover funçao

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import cipher from './cipher.js';
 
 let senhacrip = document.getElementById("senha1")
-let deslocamento = document.getElementById("desloc")
 
+let deslocamento = document.getElementById("desloc")
+function cifra (string){
+cifrado.encode()= (senhacrip.charCodeAt() - senhacrip.charCodeAt(1) + deslocamento)%25 + senhacrip.charCodeAt(1)
+return cifrado
+}
+let cifrado = cifra()
 console.log(cipher);
+document.getElementById("senhaseg").innerText = "cipher"
+
+
