bd4ccb8c4c546f67cd423b35cd0c8a4d8a03dc59
Joice Souza <joice_fsouza@hotmail.com>
2021-06-14 14:40:13 -0300

    foi implementado o JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,35 @@
-const cipher = {
-  // ...
+const cipher = {Cifra:function (senhaString, deslocamento) {
+        let novaSenha = '', cript
+        for (let i = 0; i < senhaString.length; i++) {
+            if (senhaString.charCodeAt(i) >= 65 && senhaString.charCodeAt(i)<= 90){
+             cript = (((senhaString.charCodeAt(i)-65)+deslocamento)%26)+65
+           }
+           
+            else if (senhaString.charCodeAt(i) >= 97 && senhaString.charCodeAt(i)<= 122){
+                cript = (((senhaString.charCodeAt(i)-97)+ deslocamento)%26)+97
+               
+           }
+           novaSenha += String.fromCharCode(cript)
+        }
+    return novaSenha
+},
+    Decode:function (senhaString2, deslocamento2){
+        let novaSenha2 = '', cript2
+    for (let i = 0; i < senhaString2.length; i++) {
+        if (senhaString2.charCodeAt(i) >= 65 && senhaString2.charCodeAt(i)<= 90){
+         cript2 = (((senhaString2.charCodeAt(i)-90)-deslocamento2)%26)+90
+       }
+       
+        else if (senhaString2.charCodeAt(i) >= 97 && senhaString2.charCodeAt(i)<= 122){
+            cript2 = (((senhaString2.charCodeAt(i)-122)- deslocamento2)%26)+122
+           
+       }
+       novaSenha2 += String.fromCharCode(cript2)
+    }
+return novaSenha2
+}
 };
 
 export default cipher;
+
+     
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import cipher from './cipher.js';
-
-let senhacrip = document.getElementById("senha1")
-
+let senhaString = document.getElementById("senha1")
 let deslocamento = document.getElementById("desloc")
-function cifra (string){
-cifrado.encode()= (senhacrip.charCodeAt() - senhacrip.charCodeAt(1) + deslocamento)%25 + senhacrip.charCodeAt(1)
-return cifrado
-}
-let cifrado = cifra()
-console.log(cipher);
-document.getElementById("senhaseg").innerText = "cipher"
+let CifraText = document.getElementById("resultado1")
+let button = document.getElementById("btnCrip")
+btnCrip.addEventListener('click', Cifra => {
+    Cifra.preventDefault()
+    const crypt = senhaString.value.trim()
+    const endecode = deslocamento.value
+
+    const encodeRes = btnCrip.encode(crypt, endecode)
+    CifraText.innerHTML += encodeRes
+})
+
+
+console.log(encode)
+
+
+
 
 
