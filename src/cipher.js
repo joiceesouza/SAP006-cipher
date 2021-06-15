82ab32b19ce369550c4977e81980524039b885e4
Joice Souza <joice_fsouza@hotmail.com>
2021-06-15 11:26:56 -0300

    conclusao do projeto, correçao de erro e complemento do button

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,55 @@
 const cipher = {encode:function (deslocamento, senhaString) {
         let novaSenha = ''
         let cript = ''
-        for (let i = 0; i < senhaString.length; i++) {
-            if (senhaString.charCodeAt(i) >= 65 && senhaString.charCodeAt(i)<= 90){
-             cript = (((senhaString.charCodeAt(i)-65)+deslocamento)%26)+65
-           }
-           
-            else if (senhaString.charCodeAt(i) >= 97 && senhaString.charCodeAt(i)<= 122){
-             cript = (((senhaString.charCodeAt(i)-97)+ deslocamento)%26)+97
-           }
+        if (typeof deslocamento !== "number") {
+          throw TypeError ("O deslocamente precisa ser um número")
+         }
+        else {
+          if (typeof senhaString !== "string"){
+           throw TypeError ("A senha deve ser letras!")       
+         }
+          else {  
+            for (let i = 0; i < senhaString.length; i++) {
+                if (senhaString.charCodeAt(i) >= 65 && senhaString.charCodeAt(i)<= 90){
+                cript = (((senhaString.charCodeAt(i)-65)+deslocamento)%26)+65
+              }
+              
+                else if (senhaString.charCodeAt(i) >= 97 && senhaString.charCodeAt(i)<= 122){
+                cript = (((senhaString.charCodeAt(i)-97)+ deslocamento)%26)+97
+              }
 
-           novaSenha += String.fromCharCode(cript)
+              novaSenha += String.fromCharCode(cript) 
+            }
+          }  
         }
+        
     return novaSenha
 },
     decode:function (deslocamento2, senhaString2){
         let novaSenha2 = '', cript2;
-    for (let i = 0; i < senhaString2.length; i++) {
-        if (senhaString2.charCodeAt(i) >= 65 && senhaString2.charCodeAt(i)<= 90){
-         cript2 = (((senhaString2.charCodeAt(i)-90)-deslocamento2)%26)+90
-       }
-       
-        else if (senhaString2.charCodeAt(i) >= 97 && senhaString2.charCodeAt(i)<= 122){
-         cript2 = (((senhaString2.charCodeAt(i)-122)- deslocamento2)%26)+122
-           
-       }
+        if (typeof deslocamento2 !== "number") {
+          throw TypeError ("O deslocamente precisa ser um número")
+         }
+        else {
+          if (typeof senhaString2 !== "string"){
+           throw TypeError ("A senha deve ser letras!")       
+         }
+          else {  
+            for (let i = 0; i < senhaString2.length; i++) {
+              if (senhaString2.charCodeAt(i) >= 65 && senhaString2.charCodeAt(i)<= 90){
+              cript2 = (((senhaString2.charCodeAt(i)-90)-deslocamento2)%26)+90
+            }
+            
+              else if (senhaString2.charCodeAt(i) >= 97 && senhaString2.charCodeAt(i)<= 122){
+              cript2 = (((senhaString2.charCodeAt(i)-122)- deslocamento2)%26)+122
+              
+          }
 
        novaSenha2 += String.fromCharCode(cript2)
-    }
+
+            }
+          }
+        }
 return novaSenha2
 }
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let senhaString = document.getElementById("senha1")
 let deslocamento = document.getElementById("desloc")
 let encodeText = document.getElementById("resultado1")
 let button = document.getElementById("btnCrip")
+let button2 = document.getElementById("btnCrip2")
 let crypt = ''
 let offset = ''
 senhaString.addEventListener('keyup', function(event) {
@@ -13,28 +14,18 @@ deslocamento.addEventListener('keyup', function(event) {
 } )
 
 function button1(){ 
-    let msgCifra = cipher.encode(crypt, offset);
+    let msgCifra = cipher.encode(offset, crypt);
     encodeText.innerHTML = msgCifra
 }
 
 button.addEventListener('click',button1)
 
+function button3(){ 
+    let msgCifra = cipher.decode(offset, crypt);
+    encodeText.innerHTML = msgCifra
+}
 
-
-
-
-
-
-/*btnCrip.addEventListener('click', Cifra => {
-    Cifra.preventDefault()
-    const crypt = senhaString.value.trim()
-    const endecode = deslocamento.value
-    const encodeRes = btnCrip.encode(crypt, endecode)
-    CifraText.innerHTML += encodeRes
-})*/
-
-
-/*console.log(encode)*/
+button2.addEventListener('click',button3)
 
 
 
